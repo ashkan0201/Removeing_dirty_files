125e2d07b1940e43c0cbbc60d9745a1622276a81
ashkan0201 <ashkan02011@gmial.com>
2023-09-19 02:33:21 +0330

    Create time and get user address

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ import shutil
 import winshell
 from flask import Flask , render_template , request 
 from datetime import datetime
+
+# Create time and get user address
+Time = datetime.today().ctime()
+windows_user_name = os.path.expanduser('~')
+windows_user_name = windows_user_name.replace("\\","/")
+
+
